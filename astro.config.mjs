4cf27462f87fe4b7f6d59c8a28dfb154e3e7a9e9
Adrian <adrian@zyian.dev>
2023-07-22 19:18:48 -0600

    add cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,6 +38,29 @@ export default defineConfig({
       ],
     }),
     mdx(),
+    NetlifyCMS({
+      config: {
+        // Use Netlify’s “Git Gateway” authentication and target our default branch
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Define a blog post collection
+          {
+            name: 'posts',
+            label: 'Blog Posts',
+            folder: 'src/content/blog',
+            create: true,
+            delete: true,
+            fields: [
+              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+            ],
+          },
+        ],
+      }
+    })
   ],
   markdown: {
     remarkPlugins: [
